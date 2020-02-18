1e136f2b706d37c1ca3db08a5535ac611a9996ed
carlsachs <sachscarl@gmail.com>
2020-02-18 14:28:02 -0500

    MVP

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import ToDoList from "./components/TodoList"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+    <h2>List:</h2>
+      <ToDoList />
     </div>
   );
 }
